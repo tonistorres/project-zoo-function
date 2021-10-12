54e94567c0ea6d5243c342e3d6cc535426309eeb
tonistorres <sisvenda2011@gmail.com>
2021-10-12 20:29:41 -0300

    Qt01 getSpeciesByIds Respondida

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const {
+  species,
+} = data; // base de dados que irei trabalhar
+
+function getSpeciesByIds(...ids) {
+  const arrayObj = [];
+  for (let i = 0; i < species.length; i += 1) {
+    for (let j = 0; j < ids.length; j += 1) {
+      if (species[i].id === ids[j]) {
+        arrayObj.push(species[i]);
+      }
+    }
+  }
+  return arrayObj;
 }
+
+// eslint-disable-next-line sonarjs/no-use-of-empty-return-value
+console.log(getSpeciesByIds(species[0].id, species[1].id));
 
 module.exports = getSpeciesByIds;
